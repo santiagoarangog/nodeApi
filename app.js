34342d7d86b5ec683ae99de354fc7cd287f32b3f
Santiago Arango Gutierrez <santiago.arango@experimentality.co>
2022-03-19 12:42:58 -0500

    Add structure initial API in Node JS

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const dbConnect = require('./config/mongo')
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Your app is ready in port http://localhost:${port}`)
+})
+
+dbConnect()
