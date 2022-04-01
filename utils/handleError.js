099ae683f07ad28e8db527183ab85bebad212769
Santiago Arango Gutierrez <santiago.arango@experimentality.co>
2022-04-01 07:29:18 -0500

    Fix send handle error message

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,6 @@
 const handleHttpError = (res, message = 'Sucedio un error inesperado', code = 403) => {
     res.status(code)
-    res.status({error: message})
+    res.send({error: message})
 }
 
 module.exports = {handleHttpError}
